d152e26205c62aaa30d80df9012d2ae5e34baba2
Matej Kollar <208115@mail.muni.cz>
2015-01-05 03:29:37 +0100

    Simple demo app

--- diy/testrubyserver.rb
+++ diy/testrubyserver.rb
@@ -6,9 +6,14 @@ config = {}
 config.update(:Port => 8080)
 config.update(:BindAddress => ARGV[0])
 config.update(:DocumentRoot => ARGV[1])
+config.update(:MimeTypes => {'rhtml' => 'text/html', 'css' => 'text/css'})
 server = HTTPServer.new(config)
 ['INT', 'TERM'].each {|signal|
   trap(signal) {server.shutdown}
 }
+
+# server.mount_proc '/' do |req, res|
+#   res.body = 'Hello, world!' + req.to_s
+# end
 
 server.start
